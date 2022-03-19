8d835b832daff18b9488db2ce2707136a59a05de
goodmc4 <goodmc4@nationwide.com>
2022-03-19 15:56:33 -0400

    whole bunch of logic

--- lib/models/solver.dart
+++ lib/models/solver.dart
@@ -6,12 +6,40 @@ final counts = {1: 9, 2: 9, 3: 9, 4: 9, 5: 9, 6: 9, 7: 9, 8: 9, 9: 9};
 final allDigits = int.parse('111111111', radix: 2);
 final digitsBitmask = Map.fromEntries(List.generate(9, (i) => MapEntry(i + 1, 1 << 8 - i)));
 final bitmaskDigits = Map.fromEntries(List.generate(9, (i) => MapEntry(1 << 8 - i, i + 1)));
-final twoCombos = [
+final pairs = [
   for (var i = 9; i > 0; i--)
     for (var j = i - 1; j > 0; j--) Candidates(digitsBitmask[i]! | digitsBitmask[j]!)
 ];
+final triplets = [
+  for (var i = 9; i > 0; i--)
+    for (var j = i - 1; j > 0; j--)
+      for (var k = j - 1; k > 0; k--) Candidates(digitsBitmask[i]! | digitsBitmask[j]! | digitsBitmask[k]!)
+];
+final combos = [...pairs, ...triplets];
 
 typedef CandidateState = Map<Square, Candidates>;
+
+class ListSplit<T> {
+  final Iterable<T> match;
+  final Iterable<T> rest;
+
+  ListSplit(this.match, this.rest);
+}
+
+extension SplittableList<T> on Iterable<T> {
+  ListSplit<T> split(bool Function(T) where) {
+    final List<T> match = [];
+    final List<T> rest = [];
+    for (var item in this) {
+      if (where(item)) {
+        match.add(item);
+      } else {
+        rest.add(item);
+      }
+    }
+    return ListSplit(match, rest);
+  }
+}
 
 class Candidates {
   int value;
@@ -21,6 +49,22 @@ class Candidates {
   bool has(int i) {
     return value & digitsBitmask[i]! > 0;
   }
+
+  bool hasAll(Candidates other) {
+    return other.value == value & other.value;
+  }
+
+  bool hasAny(Candidates other) {
+    return value & other.value > 0;
+  }
+
+  /// Whether there are candidates other than in `other`
+  bool hasOthers(Candidates other) {
+    return value & ~other.value > 0;
+  }
+
+  /// Has only a subset of `other` and nothing else
+  bool hasOnlyAny(Candidates other) => hasAny(other) && !hasOthers(other);
 
   Iterable<int> each() {
     return [1, 2, 3, 4, 5, 6, 7, 8, 9].where((d) => has(d));
@@ -54,9 +98,9 @@ class Candidates {
     return Candidates(value & ~other.value);
   }
 
-  // 1001101
-  //^1001000
-  // 0000101
+  bool equals(Candidates other) {
+    return value == other.value;
+  }
 
   @override
   String toString() {
@@ -65,33 +109,38 @@ class Candidates {
 }
 
 class Unit {
+  final int index;
+
   /// The squares in the unit.
-  final List<Square> squares;
+  final List<Square> squares = [];
 
   /// A mapping of digit to its count in the unit
   final Map<int, int> count = Map.from(counts);
 
-  Unit([List<Square>? squares]) : squares = squares ?? [];
-
-  /// Returns a list of the positions for a digit in this unit
-  Iterable<int> positions(int d) {
-    return squares.foldIndexed<List<int>>([], (index, positions, s) {
-      if (s.candidates.has(d)) positions.add(index);
-      return positions;
-    });
-  }
+  Unit(this.index);
 
   Iterable<Square> where(bool Function(Square) test) {
     return squares.where(test);
   }
+}
 
-  Candidates remaining() {
-    return squares.fold(Candidates(0), (previous, s) => s.candidates.union(previous));
+class Row extends Unit {
+  Row(int index) : super(index);
+
+  @override
+  String toString() {
+    return 'Row ${index + 1}';
   }
 }
 
-/// A row or column
-class Line extends Unit {}
+class Column extends Unit {
+  Column(int index) : super(index);
+
+  @override
+  String toString() {
+    return 'Col ${index + 1}';
+  }
+}
 
 class Box extends Unit {
   final int rowOffset;
@@ -99,7 +148,13 @@ class Box extends Unit {
 
   Box(int index)
       : rowOffset = (index / 3).floor() * 3,
-        colOffset = index % 3 * 3;
+        colOffset = index % 3 * 3,
+        super(index);
+
+  @override
+  String toString() {
+    return 'Box ${index + 1}';
+  }
 }
 
 class Square {
@@ -108,7 +163,6 @@ class Square {
   final int col;
   final List<Square> peers = [];
   final List<Unit> units = [];
-  Candidates candidates = Candidates();
 
   Square({required this.col, required this.row, required this.index});
 
@@ -121,9 +175,11 @@ class Square {
 class Solution {
   bool isComplete = false;
   bool isFailed = false;
-  final List<Line> rows = List.generate(9, (index) => Line());
-  final List<Line> cols = List.generate(9, (index) => Line());
+  final List<Row> rows = List.generate(9, (index) => Row(index));
+  final List<Column> cols = List.generate(9, (index) => Column(index));
   final List<Box> boxes = List.generate(9, (index) => Box(index));
+  late final List<Unit> units = [];
+  CandidateState candidates = {};
 
   /// A map of [Square]s to int representing its candidates as a bitmask
   /// 100101011 = 1..4.6.89
@@ -140,8 +196,14 @@ class Solution {
       cols[col].squares.add(square);
       boxes[box].squares.add(square);
       square.units.addAll([rows[row], cols[col], boxes[box]]);
+      candidates[square] = Candidates(allDigits);
       return square;
     });
+
+    units
+      ..addAll(rows)
+      ..addAll(cols)
+      ..addAll(boxes);
 
     for (var square in squares) {
       square.peers
@@ -158,152 +220,248 @@ class Solution {
 
     for (int i = 0; i < clues.length; i++) {
       if (clues.elementAt(i) > 0) {
-        assign(squares[i], clues.elementAt(i));
-        if (isFailed) break;
+        if (assign(candidates, squares[i], clues.elementAt(i)) == null) break;
       }
     }
   }
 
-  Solution copy() {
-    return Solution._internal(squares.map((s) => s.candidates.isSingle ? bitmaskDigits[s.candidates.value]! : 0));
-  }
-
   factory Solution.fromString(String grid) {
     final g = grid.replaceAll('.', '0').replaceAll(RegExp(r'[^\d]'), '').split('').map((d) => int.parse(d));
-    return Solution._internal(g);
+    return Solution._internal(g)..solve();
   }
 
-  void assign(Square s, int d) {
-    final others = s.candidates.remove(d);
+  CandidateState? assign(CandidateState candidates, Square s, int d) {
+    final others = candidates[s]!.remove(d);
     for (var d2 in others.each()) {
-      eliminate(s, d2);
-      if (isFailed) return;
+      if (eliminate(candidates, s, d2) == null) return null;
     }
+    return candidates;
   }
 
-  void eliminate(Square s, int d) {
-    var c = s.candidates;
+  CandidateState? eliminate(CandidateState candidates, Square s, int d) {
+    var c = candidates[s]!;
     // Already removed
-    if (!c.has(d)) return;
-    c = s.candidates = c.remove(d);
+    if (!c.has(d)) return candidates;
+    c = candidates[s] = c.remove(d);
     // All candidates removed, a contradiction
-    if (c.isEmpty) isFailed = true;
+    if (c.isEmpty) {
+      print('PROBLEM???');
+      return null;
+    }
     // Only one candidate remains, propagate its removal from peers
     if (c.isSingle) {
       for (var peer in s.peers) {
-        eliminate(peer, bitmaskDigits[c.value]!);
-        if (isFailed) return;
+        if (eliminate(candidates, peer, bitmaskDigits[c.value]!) == null) return null;
       }
     }
 
     // Check if the square's units now only have 1 place d can be put
     for (final unit in s.units) {
-      final dPlaces = unit.where((s) => s.candidates.has(d));
+      final dPlaces = unit.where((s) => candidates[s]!.has(d));
       if (dPlaces.isEmpty) {
         isFailed = true;
       } else if (dPlaces.length == 1) {
-        assign(dPlaces.first, d);
-        if (isFailed) return;
+        if (assign(candidates, dPlaces.first, d) == null) return null;
       }
     }
+
+    return candidates;
   }
 
-  void candidateLines() {
-    if (isFailed || isSolved) return;
+  CandidateState? nakedSubset(CandidateState? candidates) {
+    if (candidates == null) return null;
+    if (isSolved(candidates)) return candidates;
+
+    for (var unit in units) {
+      for (var combo in combos) {
+        var matches = <Square>[];
+        for (var s in unit.squares) {
+          if (!candidates[s]!.isSingle && candidates[s]!.hasOnlyAny(combo)) matches.add(s);
+        }
+
+        if (matches.length == combo.length) {
+          var affected = unit.squares.where((s) => !matches.contains(s));
+          if (union(candidates, affected).hasAny(combo)) {
+            print('Naked subset $combo found in $unit');
+            display(candidates);
+            for (var s in affected) {
+              for (var d in combo.each()) {
+                if (eliminate(candidates, s, d) == null) return null;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    return candidates;
+  }
+
+  CandidateState? hiddenSubset(CandidateState? candidates) {
+    if (candidates == null) return null;
+    if (isSolved(candidates)) return candidates;
+
+    for (var unit in units) {
+      for (var combo in combos) {
+        var matches = <Square>[];
+        // This needs to end up containing the combo. e.g. we union up the subset
+        // matches, so we make sure it doesn't just find 1,1,1 as a "triplet". It
+        // also tells us when its length > combo.length that there were other
+        // candidates present that prove it was "hidden."
+        var foundCandidates = Candidates(0);
+
+        for (var s in unit.squares) {
+          if (!candidates[s]!.isSingle && candidates[s]!.hasAny(combo)) {
+            matches.add(s);
+            foundCandidates = foundCandidates.union(candidates[s]!);
+          }
+        }
+
+        if (matches.length == combo.length && foundCandidates.hasAll(combo) && !foundCandidates.equals(combo)) {
+          print('Hidden subset $combo found in $unit');
+          display(candidates);
+          for (var s in matches) {
+            for (var d = 1; d <= 9; d++) {
+              if (!combo.has(d)) {
+                if (eliminate(candidates, s, d) == null) return null;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    return candidates;
+  }
+
+  CandidateState? pointingPairs(CandidateState? candidates) {
+    if (candidates == null) return null;
+    if (isSolved(candidates)) return candidates;
 
     for (var b = 0; b < boxes.length; b++) {
       final box = boxes[b];
-      // A list where the index corresponds to a digit, and the values is 0,2,3,4 depending
-      // on whether the digit is ONLY in row/col none, first, second, third of the box
+      // A list where the index corresponds to a digit, and the values tell which row/col it's in
+      // 0, single-row: 2, 3, 4 multi-row: 5, 6, 7, 9
       final dRows = List.filled(9, 0);
       final dCols = List.filled(9, 0);
       for (var i = 0; i < 3; i++) {
-        // all the candidates in this row of this box
-        final row = union(box.where((s) => s.row % 3 == i && !s.candidates.isSingle));
-        // all the candidates in this col of this box
-        final col = union(box.where((s) => s.col % 3 == i && !s.candidates.isSingle));
-        for (var d = 1; d <= 9; d++) {
-          if (row.has(d)) dRows[d - 1] += i + 2;
-          if (col.has(d)) dCols[d - 1] += i + 2; // 0, 2, 3, 4 : 5, 6, 7
+        for (var isRow in [true, false]) {
+          final squares = box.squares.split((s) => (isRow ? s.row : s.col) % 3 == i && !candidates[s]!.isSingle);
+          final boxLine = union(candidates, squares.match);
+          final dLines = isRow ? dRows : dCols;
+          for (var d = 1; d <= 9; d++) {
+            if (boxLine.has(d)) {
+              dLines[d - 1] += i + 2;
+              var restOfBox = box.where((s) => !squares.match.contains(s));
+              if (!union(candidates, squares.rest).has(d) && union(candidates, restOfBox).has(d)) {
+                print('Box/Line reduction $d in ${isRow ? 'row' : 'col'} ${i + 1} of $box');
+                display(candidates);
+                for (var s in restOfBox) {
+                  if (eliminate(candidates, s, d) == null) return null;
+                }
+              }
+            }
+          }
         }
       }
       for (var d = 1; d <= 9; d++) {
         List<Square> affected = [];
-        if ([2, 3, 4].contains(dRows[d - 1])) {
-          final rest = rows[dRows[d - 1] - 2 + (b / 3).floor() * 3].where((s) => !s.units.contains(box));
-          if (union(rest).has(d)) {
-            affected.addAll(rest);
-            print('candidate row ${dRows[d - 1] - 2}, box $b, digit $d');
-            // display();
+        for (var isRow in [true, false]) {
+          final lines = isRow ? rows : cols;
+          final dLines = isRow ? dRows : dCols;
+          final offset = isRow ? box.rowOffset : box.colOffset;
+          final index = dLines[d - 1] - 2;
+
+          // If d is only in one row/col of the box it's a pointing pair
+          if ([2, 3, 4].contains(dLines[d - 1])) {
+            var rest = lines[index + offset].where((s) => !s.units.contains(box));
+            if (union(candidates, rest).has(d)) {
+              affected.addAll(rest);
+              print('Pointing pair $d in ${isRow ? 'row' : 'col'} ${index + 1} of $box');
+              display(candidates);
+            }
           }
+          // If d isn't just in this row/col of the box but it *is* the only ds
+          // in the whole row/col, it's a box/line reduction
+          // else if(!dIsInRest) {
+          //     rest = box.where((s) => s.row != index + box.rowOffset);
+          //     if (union(candidates, rest).has(d)) {
+          //       affected.addAll(rest);
+          //       print('Box/Line reduction $d in ${isRow ? 'row': 'col'} ${index + 1} of $box');
+          //       display(candidates);
+          //     }
+          //   }
         }
-        if ([2, 3, 4].contains(dCols[d - 1])) {
-          final rest = cols[dCols[d - 1] - 2 + b % 3 * 3].where((s) => !s.units.contains(box));
-          if (union(rest).has(d)) {
-            affected.addAll(rest);
-            print('candidate col ${dCols[d - 1] - 2}, box $b, digit $d');
-            // display();
-          }
-        }
+
         for (var s in affected) {
-          eliminate(s, d);
+          if (eliminate(candidates, s, d) == null) return null;
         }
       }
     }
+
+    return candidates;
   }
 
-  Candidates union(Iterable<Square> squares) {
-    return squares.fold<Candidates>(Candidates(0), (previous, s) => s.candidates.union(previous));
+  Candidates union(CandidateState candidates, Iterable<Square> squares) {
+    return squares.fold<Candidates>(Candidates(0), (previous, s) => candidates[s]!.union(previous));
   }
 
-  void display() {
-    final width = 2 + (squares.map((s) => s.candidates.length)).reduce(max);
+  void display(CandidateState candidates) {
+    final width = 2 + (squares.map((s) => candidates[s]!.length)).reduce(max);
     final line = '\n' + List.filled(3, List.filled(width * 3, '-').join('')).join('+');
     var result = '';
     for (var s in squares) {
-      final val = s.candidates.toString();
+      final val = candidates[s]!.toString();
       final pad = (width - val.length) / 2;
       result += val.padLeft(val.length + pad.ceil()).padRight(width);
       if (s.col == 2 || s.col == 5) result += '|';
       if ((s.row == 2 || s.row == 5) && s.col == 8) result += line;
       if (s.col == 8) result += '\n';
     }
-    print(result);
+    print(result + '\n\n\n.');
   }
 
-  bool get isSolved {
-    return squares.every((s) => s.candidates.isSingle);
+  bool isSolved(CandidateState candidates) {
+    return candidates.values.every((c) => c.isSingle);
   }
-}
 
-solve(String grid) {
-  final stopwatch = Stopwatch()..start();
-  final result = search(Solution.fromString(grid));
-  stopwatch.stop();
-  if (result == null) {
-    print('Unsolvable');
-  } else {
-    result.display();
+  solve() {
+    final stopwatch = Stopwatch()..start();
+    final result = search(applyLogic(candidates));
+    stopwatch.stop();
+    if (result == null) {
+      print('Unsolvable');
+    } else {
+      print('RESULT:');
+      display(result);
+    }
+    print(stopwatch.elapsed);
   }
-  print(stopwatch.elapsed);
-}
 
-Solution? search(Solution solution, [int n = 0]) {
-  // Already failed or solved
-  if (solution.isFailed) return null;
-  if (solution.isSolved) return solution;
-
-  // Pick the cell with the least remaining candidates and try out each
-  final s = solution.squares
-      .where((s) => s.candidates.length > 1)
-      .sorted((a, b) => a.candidates.length - b.candidates.length)
-      .first;
-  for (var d in s.candidates.each()) {
-    var newSolution = solution.copy();
-    newSolution.assign(newSolution.squares[s.index], d);
-    newSolution.candidateLines();
-    var result = search(newSolution, n + 1);
-    if (result != null) return result;
+  CandidateState? applyLogic(CandidateState? candidates) {
+    return hiddenSubset(nakedSubset(pointingPairs(candidates)));
   }
-  return null;
+
+  CandidateState? search(CandidateState? candidates, [int n = 0]) {
+    if (candidates == null) return null;
+    if (isSolved(candidates)) return candidates;
+    print('Search $n');
+
+    // Pick the cell with the least remaining candidates and try out each
+    final x = squares.where((s) => candidates[s]!.length > 1);
+
+    final s = x.sorted((a, b) => candidates[a]!.length - candidates[b]!.length).first;
+
+    for (var d in candidates[s]!.each()) {
+      print('Trying $d for $s');
+      var result = search(applyLogic(assign(copy(candidates), s, d)), n + 1);
+      print('Up to $n');
+      if (result != null) return result;
+    }
+    return null;
+  }
+
+  CandidateState copy(CandidateState candidates) {
+    return candidates.map((s, c) => MapEntry(s, Candidates(c.value)));
+  }
 }
